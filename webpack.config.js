1f8a68b209544343076dbe4e6ddecc66a705abdb
lena <lena@MacBook-Air-Alexandr.local>
2023-09-25 19:24:58 +0400

    add webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,35 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import path, { dirname } from 'node:path';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
+export default {
+  mode: process.env.NODE_ENV || 'development',
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
-    path: __dirname + '/dist',
+    path: path.resolve(dirname('dist')),
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: 'index.html',
     }),
   ],
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
 };
